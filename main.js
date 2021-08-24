4c6d76d1bb02217fa768afa4289709ee52199651
Amaan-Fuad <75928667+Amaan-Fuad@users.noreply.github.com>
2021-08-24 16:58:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+prediction1="";
+prediction2="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+function capture(){
+    Webcam.snap(function(selfie){
+        document.getElementById("result").innerHTML="<img src='"+selfie+"' id='capturedimg'>";
+    });
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/u8baSrTnX/model.json',modelloaded);
+function modelloaded(){
+    console.log("model loaded");
+}
+function check(){
+    img=document.getElementById("capturedimg");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,results){
+if(error){
+    console.log("error");
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name1").innerHTML=results[0].label;
+    prediction1=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction2=results[1].label;
+    speak();
+}
+if(results[0].label=="Victory"){
+    document.getElementById("update_emoji1").innerHTML="&#9996;";
+}
+if(results[1].label=="Victory"){
+    document.getElementById("update_emoji2").innerHTML="&#9996;";
+}
+if(results[0].label=="Amazing"){
+    document.getElementById("update_emoji1").innerHTML="&#128076;";
+}
+if(results[1].label=="Amazing"){
+    document.getElementById("update_emoji2").innerHTML="&#128076;";
+}
+if(results[0].label=="Thumbs-down"){
+    document.getElementById("update_emoji1").innerHTML="&#128078;";
+}
+if(results[1].label=="Thumbs-down"){
+    document.getElementById("update_emoji2").innerHTML="&#128078;";
+}
+if(results[0].label=="Thumbs-up"){
+    document.getElementById("update_emoji1").innerHTML="&#128077;";
+}
+if(results[1].label=="Thumbs-up"){
+    document.getElementById("update_emoji2").innerHTML="&#128077;";
+}
+}
+function speak(){
+synth=window.speechSynthesis;
+speak1="The first prediction is"+prediction1;
+speak2="The second prediction is"+prediction2;
+utterthis=new SpeechSynthesisUtterance(speak1+speak2);
+synth.speak(utterthis);
+}
